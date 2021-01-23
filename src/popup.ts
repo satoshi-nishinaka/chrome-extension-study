f53d998364f7453ee2a13b8e5271c3c8f8f6c9a7
Satoshi Nishinaka <satoshie.sp@gmail.com>
2021-01-23 19:28:26 +0900

    不要なAlertの削除

--- src/popup.ts
+++ src/popup.ts
@@ -13,7 +13,6 @@ function transitionToNextPage(url: string, newTab: boolean): void {
   if (url === undefined) {
     return
   }
-  alert(url)
   if (newTab) {
     chrome.tabs.create({ url: url });
     return;
@@ -21,9 +20,7 @@ function transitionToNextPage(url: string, newTab: boolean): void {
 
   // Get the current Tab
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-
     const active = tabs[0].id;
-
     // Set the URL to the Local-NTP (New Tab Page)
     chrome.tabs.update(active, { url: url }, function () { });
   });
