99aedf7a112cc6997471833fd2ec048ded7510fc
Satoshi Nishinaka <satoshie.sp@gmail.com>
2021-09-26 19:35:03 +0900

    class名にヒットしてしまうので何らかの対策を考える必要がある

--- src/content_script.ts
+++ src/content_script.ts
@@ -2,26 +2,37 @@ import * as $ from 'jquery';
 import { Storage } from './Storage';
 import { unique } from './Functions/Unique';
 
+const ignoreTags = [
+    'head',
+    'script',
+    'meta',
+]
+
 const storage = new Storage();
 storage.readValues(() => {
   if (storage.enableHighlight === false || !storage.highlightWords) {
     return;
   }
+  console.debug('start replaceHighlightText ');
   const highlightWords = unique(storage.highlightWords.trim().split('\n'));
   const highlight = (highlightWords: string[]): void => {
-    const target = $('body')[0];
-    let html = target.innerHTML;
-    console.debug('start replaceHighlightText ');
-    highlightWords.map((value) => {
-      const regExp = new RegExp(`(${value})`, 'g');
-      html = html.replace(
-        regExp,
-        '<span class="shortcut-extension-highlight">$1</span>'
-      );
-    });
+    $('*').map((index, domElement) => {
+      let html = domElement.innerHTML;
+      console.debug(domElement.tagName)
+      if (ignoreTags.includes(domElement.tagName)) {
+        return
+      }
+      highlightWords.map((value) => {
+        const regExp = new RegExp(`(${value})`, 'g');
+        html = html.replace(
+          regExp,
+          '<span class="shortcut-extension-highlight">$1</span>'
+        );
+      });
 
-    target.innerHTML = html;
-    console.log('replace finish');
+      domElement.innerHTML = html;
+    });
+    console.debug('replace finish');
   };
   highlight(highlightWords);
 });
