178a7886d8115d39974bdb30d2c5b2ed22e7a723
satoshi-nishinaka <satoshie.sp@gmail.com>
2021-09-28 19:28:21 +0900

    textだけ書き換えにはならないのでもうひと工夫必要

--- src/content_script.ts
+++ src/content_script.ts
@@ -2,11 +2,8 @@ import * as $ from 'jquery';
 import { Storage } from './Storage';
 import { unique } from './Functions/Unique';
 
-const ignoreTags = [
-    'head',
-    'script',
-    'meta',
-]
+// そもそもチェックしなくて良いタグは最初から弾く
+const ignoreTags = ['head', 'script', 'meta', 'svg', 'img'];
 
 const storage = new Storage();
 storage.readValues(() => {
@@ -17,12 +14,25 @@ storage.readValues(() => {
   const highlightWords = unique(storage.highlightWords.trim().split('\n'));
   const highlight = (highlightWords: string[]): void => {
     $('*').map((index, domElement) => {
-      let html = domElement.innerHTML;
-      console.debug(domElement.tagName)
-      if (ignoreTags.includes(domElement.tagName)) {
-        return
+      if (ignoreTags.includes(domElement.tagName.toLowerCase())) {
+        return;
       }
+      if (
+        domElement.hasChildNodes() &&
+        (domElement.childNodes.length !== 1 ||
+          domElement.childNodes[0].nodeName !== '#text')
+      ) {
+        // 子要素のAttributeを書き換えてしまうケースがあるので子要素がある場合はスキップ
+        // テキスト要素もhasChildNodes === trueになる
+        return;
+      }
+
+      let html = domElement.innerHTML;
+      const text = domElement.innerText;
       highlightWords.map((value) => {
+        if (!text || text.indexOf(value) === -1) {
+          return;
+        }
         const regExp = new RegExp(`(${value})`, 'g');
         html = html.replace(
           regExp,
