db5297f5403b3ac2e4f448f7686c74ed0e7da052
satoshi-nishinaka <satoshie.sp@gmail.com>
2022-09-01 18:22:06 +0900

    テストケースを追加

--- src/Functions/__tests__/Unique.ts
+++ src/Functions/__tests__/Unique.ts
@@ -5,3 +5,15 @@ test('remove duplicate element', () => {
   const expected = ['A', 'B', 'AA'];
   expect(unique(input)).toStrictEqual(expected);
 });
+
+test('remove duplicate elements', () => {
+  const input = ['A', 'B', 'AA', 'B', 'AA', 'A', 'C'];
+  const expected = ['A', 'B', 'AA', 'C'];
+  expect(unique(input)).toStrictEqual(expected);
+});
+
+test('remove duplicate elements', () => {
+  const input = ['A', 'a', 'A', 'b', 'b', 'C', 'C'];
+  const expected = ['A', 'a', 'b', 'C'];
+  expect(unique(input)).toStrictEqual(expected);
+});
