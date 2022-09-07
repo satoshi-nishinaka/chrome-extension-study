39df5b7e4f267624c881737b043537557e88ce3f
satoshi-nishinaka <satoshie.sp@gmail.com>
2022-09-07 10:44:12 +0900

    React-Testing-Libraryを使ったテストを追加

--- src/Component/__tests__/LinkButton.tsx
+++ src/Component/__tests__/LinkButton.tsx
@@ -1,9 +1,10 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import * as React from 'react';
-import { render, screen } from '@testing-library/react';
-import { LinkButton } from '../LinkButton';
 
-test('rendering button', async () => {
-  render(<LinkButton text={'test link button'} url={'https://example.com/'} />);
-
-  expect(await screen.findByText('test link button')).toBeVisible();
+test('use jsdom in this test file', () => {
+  const element = document.createElement('div');
+  expect(element).not.toBeNull();
 });
