3311d97534d16b508b04c22d3100570961c8848b
Satoshi Nishinaka <satoshie.sp@gmail.com>
2022-09-17 15:40:58 +0900

    React-Testing-Libraryでのテストを追加

--- src/Component/__tests__/LinkButton.tsx
+++ src/Component/__tests__/LinkButton.tsx
@@ -3,8 +3,18 @@
  */
 
 import * as React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { LinkButton } from '../LinkButton';
 
 test('use jsdom in this test file', () => {
   const element = document.createElement('div');
   expect(element).not.toBeNull();
+});
+
+test('renders a text', () => {
+  const { getByText } = render(
+    <LinkButton text={'hoge'} url={'https://example.com'} />
+  );
+  expect(getByText('hoge')).toBeInTheDocument();
 });
