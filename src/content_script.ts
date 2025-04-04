be3e057bf2f34f1f805aa08a3507eb83c2587fc5
satoshi-nishinaka <satoshie.sp@gmail.com>
2025-04-04 12:30:08 +0900

    ハイライト機能の実行タイミングをページのonLoadのタイミングに変更

--- src/content_script.ts
+++ src/content_script.ts
@@ -7,13 +7,14 @@ const ignoreTags = [
   'head',
   'title',
   'script',
+  'source',
   'style',
   'meta',
   'svg',
   'img',
 ];
 
-console.info('short-cut-extension start');
+console.info('---- short-cut-extension start ----');
 
 /**
  * BackgroundScriptからメッセージを受け取る
@@ -35,62 +36,74 @@ chrome.runtime.onMessage.addListener((request) => {
   alert('現在開いているページのタイトルとURLをクリップボードにコピーしました');
 });
 
-const storage = new Storage();
-storage.readValues().then(() => {
+const highlight = (storage: Storage): void => {
   if (storage.enableHighlight === false || !storage.highlightWords) {
     return;
   }
   const highlightWords = unique(storage.highlightWords.trim().split('\n'));
-  const highlight = (highlightWords: string[]): void => {
-    const elements = document.querySelectorAll('*');
-    elements.forEach((element) => {
-      if (ignoreTags.includes(element.tagName.toLowerCase())) {
-        return;
-      }
-      if (
-        element.hasChildNodes() &&
-        (element.childNodes.length !== 1 ||
-          element.childNodes[0].nodeName !== '#text')
-      ) {
-        // 子要素のAttributeを書き換えてしまうケースがあるので子要素がある場合はスキップ
-        // テキスト要素もhasChildNodes === trueになる
-        return;
-      }
-      const htmlElement = element as HTMLElement;
-      let html = element.innerHTML;
-      const text = htmlElement.innerText;
-      highlightWords.map((value) => {
-        if (!text || text.indexOf(value) === -1) {
-          return;
-        }
-        const regExp = new RegExp(`(${value})`, 'g');
-        html = html.replace(
-          regExp,
-          '<span class="shortcut-extension-highlight">$1</span>'
-        );
-      });
-
-      htmlElement.innerHTML = html;
-    });
-    if (storage.enableConsoleLog) {
-      console.debug('replace finish');
-    }
-    if (storage.showSucceedMessage) {
-      const label = document.createElement('span');
-      label.innerHTML = 'Highlight text';
-      label.className = 'shortcut-extension-succeed-message';
-      label.addEventListener('click', () => {
-        console.debug('clicked');
-        const label = document.getElementsByClassName(
-          'shortcut-extension-succeed-message'
-        )[0] as HTMLElement;
-        label.style.display = 'none';
-      });
-      // document.appendChild(label);
-    }
-  };
   if (storage.enableConsoleLog) {
     console.debug('start replaceHighlightText ');
   }
-  highlight(highlightWords);
-});
+  const elements = document.querySelectorAll('*');
+  elements.forEach((element) => {
+    if (ignoreTags.includes(element.tagName.toLowerCase())) {
+      return;
+    }
+    if (
+      element.hasChildNodes() &&
+      (element.childNodes.length !== 1 ||
+        element.childNodes[0].nodeName !== '#text')
+    ) {
+      // 子要素のAttributeを書き換えてしまうケースがあるので子要素がある場合はスキップ
+      // テキスト要素もhasChildNodes === trueになる
+      return;
+    }
+    const htmlElement = element as HTMLElement;
+    let html = element.innerHTML;
+    const text = htmlElement.innerText;
+    highlightWords.map((value) => {
+      if (!text || text.indexOf(value) === -1) {
+        return;
+      }
+      const regExp = new RegExp(`(${value})`, 'g');
+      html = html.replace(
+        regExp,
+        '<span class="shortcut-extension-highlight">$1</span>'
+      );
+    });
+
+    htmlElement.innerHTML = html;
+  });
+  if (storage.enableConsoleLog) {
+    console.debug('replace finish');
+  }
+  if (storage.showSucceedMessage) {
+    const label = document.createElement('span');
+    label.innerHTML = 'Highlight text';
+    label.className = 'shortcut-extension-succeed-message';
+    label.addEventListener('click', () => {
+      console.debug('clicked');
+      const label = document.getElementsByClassName(
+        'shortcut-extension-succeed-message'
+      )[0] as HTMLElement;
+      label.style.display = 'none';
+    });
+    // document.appendChild(label);
+  }
+};
+
+const storage = new Storage();
+window.onload = () => {
+  if (storage.enableConsoleLog) {
+    console.debug('---- short-cut-extension window.onload --');
+  }
+  storage
+    .readValues()
+    .then(() => {
+      highlight(storage);
+    })
+    .catch((e) => {
+      console.error(e);
+      alert('設定の読み込みに失敗しました');
+    });
+};
